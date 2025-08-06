a1ca9dd57ca0749b7de3b9d6d08fef4a89b04013
Deepanjali Arya <deepanjaliarya21@proton.me>
2025-08-06 13:31:24 +0000

    rotating camera

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import WrapButton from "@/components/ui/wrap-button";
 import * as React from "react";
+import { Camera } from "lucide-react";
+import WrapButton from "@/components/ui/wrap-button";
 
 const Page = () => {
   return (
     <div className="min-h-screen w-full flex justify-center align-center bg-gray-900">
       <WrapButton href="https://skiper-ui.com" className="font-teachers">
+        <Camera className="animate-spin" />
         Welcome to Skiper
       </WrapButton>
     </div>
