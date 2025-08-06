76de577e0058ef2afa6936918e8ad5d84f0849e6
Deepanjali Arya <deepanjaliarya21@proton.me>
2025-08-06 13:18:12 +0000

    test skiper wrap button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import WrapButton from "@/components/ui/wrap-button";
 import * as React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <p className="font-extralight text-red-500">Hello everyone!</p>
+    <div className="min-h-screen w-full flex justify-center align-center bg-gray-900">
+      <WrapButton href="https://skiper-ui.com">Welcome to Skiper</WrapButton>
     </div>
   );
 };
