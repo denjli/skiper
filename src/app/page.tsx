3a1631e119f4bce1f0786061a5c35977386ef18a
Deepanjali Arya <deepanjaliarya21@proton.me>
2025-08-06 21:43:52 +0000

    back to school hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,35 @@
 "use client";
 
 import * as React from "react";
-import FlipLink from "@/components/ui/text-effect-flipper";
+import ImageCursorTrail from "@/components/ui/image-cursortrail";
+
+// TODO: Add stationary images from pinterest
+const images = [
+  "https://images.pexels.com/photos/30082445/pexels-photo-30082445.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.unsplash.com/photo-1692606743169-e1ae2f0a960f?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1709949908058-a08659bfa922?q=80&w=1200&auto=format",
+  "https://images.unsplash.com/photo-1548192746-dd526f154ed9?q=80&w=1200&auto=format",
+  "https://images.unsplash.com/photo-1644141655284-2961181d5a02?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.pexels.com/photos/30082445/pexels-photo-30082445.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://assets.lummi.ai/assets/QmNfwUDpehZyLWzE8to7QzgbJ164S6fQy8JyUWemHtmShj?auto=format&w=1500",
+  "https://images.unsplash.com/photo-1706049379414-437ec3a54e93?q=80&w=1200&auto=format",
+  "https://assets.lummi.ai/assets/Qmb2P6tF2qUaFXnXpnnp2sk9HdVHNYXUv6MtoiSq7jjVhQ?auto=format&w=1500",
+  "https://images.unsplash.com/photo-1508873881324-c92a3fc536ba?q=80&w=1200&auto=format",
+];
 
 const Page = () => {
   return (
-    <div className="min-h-screen w-full flex justify-center align-center font-teachers">
-      <FlipLink href="https://hackclub.com">Hackathon</FlipLink>
+    <div className="min-h-screen w-full flex justify-center align-center">
+      {/* TODO: Change the settings later on */}
+      <ImageCursorTrail
+        items={images}
+        distance={25}
+        imgClass="sm:w-40 w-28 sm:h-48 h-36"
+      >
+        <h1 className="max-w-2xl text-center text-5xl font-semibold tracking-tight ">
+          Back to School in Style
+        </h1>
+      </ImageCursorTrail>
     </div>
   );
 };
