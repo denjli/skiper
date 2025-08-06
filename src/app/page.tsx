58e1c9da13b718ddce94ff47400085335b6b5ed8
Deepanjali Arya <deepanjaliarya21@proton.me>
2025-08-06 13:27:43 +0000

    add teachers font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import * as React from "react";
 const Page = () => {
   return (
     <div className="min-h-screen w-full flex justify-center align-center bg-gray-900">
-      <WrapButton href="https://skiper-ui.com">Welcome to Skiper</WrapButton>
+      <WrapButton href="https://skiper-ui.com" className="font-teachers">
+        Welcome to Skiper
+      </WrapButton>
     </div>
   );
 };
