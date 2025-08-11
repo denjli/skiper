4c416ae4e9e7d4b3d8bb53401f4c27408cad12ca
Deepanjali Arya <deepanjaliarya21@proton.me>
2025-08-11 16:01:52 +0000

    change text to light japanese

--- src/components/Japanese.tsx
+++ src/components/Japanese.tsx
@@ -3,8 +3,8 @@ import { TextScroll } from "./ui/text-scroll";
 const Japanese = () => {
   return (
     <TextScroll
-      className="font-display text-center text-4xl font-semibold tracking-tighter  text-black dark:text-white md:text-7xl md:leading-[5rem] font-noto-jp"
-      text="文 豪 が 学 校 に 戻 る I    B u n G o  B a c k  T o  S c h o o l    "
+      className="font-display text-center text-4xl font-extralight tracking-tighter  text-black dark:text-white md:text-7xl md:leading-[5rem] font-noto-jp"
+      text="文 豪 が 学 校 に 戻 る I    B U N G O    "
       default_velocity={5}
     />
   );
