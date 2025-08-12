e17941f4c80abf1e13eed086450a295f9b5b5e0f
Deepanjali Arya <deepanjaliarya21@proton.me>
2025-08-12 21:08:16 +0000

    remove unnecessary things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import * as React from "react";
-
-import { FluidCursor } from "react-cool-cursors"; // or FluidCursor
-import "react-cool-cursors/dist/style.css";
-
 import Hero from "@/components/Hero";
 import Products from "@/components/Products";
 import Japanese from "@/components/Japanese";
@@ -15,8 +11,6 @@ import Share from "@/components/Share";
 const Page = () => {
   return (
     <div className="min-h-screen w-full">
-      <FluidCursor size={25} opacity={0.5} />
-
       <Hero />
 
       <div className="w-full pt-10">
